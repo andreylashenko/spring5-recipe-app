d10cd897c32d44923d0a5f1694cf6aa8a883d393
andreylashenko <andreylashenko@mail.ru>
2019-02-16 20:21:13 +0300

    add tests, lombok

--- src/main/java/guru/springframework/controllers/indexController.java
+++ src/main/java/guru/springframework/controllers/indexController.java
@@ -1,4 +1,26 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
 public class indexController {
+
+    private  final RecipeService recipeService;
+
+    public indexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        log.debug("Getting index");
+        model.addAttribute("recipes", recipeService.getRecipes());
+
+        return "index";
+    }
 }
